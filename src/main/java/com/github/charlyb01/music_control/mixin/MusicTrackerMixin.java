ad9af508ad9dea04988b8a1e522a6081864f1815
joshxx312 <joshua.progamer300012@gmail.com>
2026-03-06 15:28:20 +0100

    Add import for MusicSound in MusicTrackerMixin

--- src/main/java/com/github/charlyb01/music_control/mixin/MusicTrackerMixin.java
+++ src/main/java/com/github/charlyb01/music_control/mixin/MusicTrackerMixin.java
@@ -23,6 +23,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.sound.MusicSound;
 
 import java.util.HashSet;
 
